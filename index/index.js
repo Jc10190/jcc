9248455b2f447ba896dd09bf51b3793624403188
Jc10190 <107447359+Jc10190@users.noreply.github.com>
2022-06-24 08:53:08 +0800

    Add files via upload

---
+++ index/index.js
@@ -0,0 +1,59 @@
+const app = getApp();
+
+Page({
+  data:{
+    sites:[{
+        state:"开启",
+        _timestamp:0,
+        _humanExist:false,
+        _bookExist:false,
+        _occupied:false,
+      },
+      {
+        state:"关闭",
+        _timestamp:0,
+        _humanExist:false,
+        _bookExist:false,
+        _occupied:false,
+      },
+      {
+        state:"关闭",
+        _timestamp:0,
+        _humanExist:false,
+        _bookExist:false,
+        _occupied:false,
+      },
+      {
+        state:"关闭",
+        _timestamp:0,
+        _humanExist:false,
+        _bookExist:false,
+        _occupied:false,
+      },
+      {
+        state:"关闭",
+        _timestamp:0,
+        _humanExist:false,
+        _bookExist:false,
+        _occupied:false,
+      },
+      {
+        state:"关闭",
+        _timestamp:0,
+        _humanExist:false,
+        _bookExist:false,
+        _occupied:false,
+      },
+    ],
+    _state:"未选择",
+    _timestamp:0,
+    _humanExist:null,
+    _bookExist:null,
+    _occupied:null,
+    chosen:0,
+  },
+
+  
+  
+
+})
